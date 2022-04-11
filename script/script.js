b7a0e47d487cc87574c1d3e8bae0458d14674420
ryrd <rdridhani@gmail.com>
2022-04-12 06:51:36 +0800

    spent-add

--- script/script.js
+++ script/script.js
@@ -1,50 +1,96 @@
 "use strict";
-// reusable function
-const closeAddReset = () => {
-    addReset.classList.toggle("hidden");
-    addReset.classList.toggle("flex");
+const mainText = document.querySelector('#main-text');
+const todaySpentInput = document.querySelector('#today-spent-input');
+const addPeriod = (x) => {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return parts.join("");
 };
-const closePopup = () => {
-    popup.classList.toggle("hidden");
-    popup.classList.toggle("flex");
-};
-//---------show add reset--------
+let moneyLeft = 0;
+if (localStorage.getItem('money-left') !== null) {
+    mainText.innerText = addPeriod(parseInt(localStorage.getItem('money-left')));
+    moneyLeft = parseInt(localStorage.getItem('money-left'));
+}
+const form = document.querySelector('#today-spent-form');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    if (todaySpentInput.value) {
+        localStorage.setItem('new-spent', todaySpentInput.value);
+        moneyLeft -= parseInt(todaySpentInput.value);
+        mainText.innerText = addPeriod(moneyLeft);
+        localStorage.setItem('money-left', moneyLeft.toString());
+    }
+    else {
+        console.log('kosong');
+    }
+    ;
+});
+const incomeForm = document.querySelector('#income-form');
+const incomeInput = document.querySelector('#income-input');
+incomeForm.addEventListener('submit', e => {
+    e.preventDefault();
+    if (incomeInput.value) {
+        moneyLeft += parseInt(incomeInput.value);
+        mainText.innerText = addPeriod(moneyLeft);
+        localStorage.setItem('money-left', moneyLeft.toString());
+        toggleAddReset();
+    }
+    else {
+        console.log('kosong');
+    }
+    ;
+});
+//---------------------------------------------------------------------
+//---------------------------------------------------------------------
 const showAddReset = document.querySelector('#show-add');
 const addReset = document.querySelector('#add-reset');
-showAddReset.addEventListener('click', () => {
-    addReset.classList.toggle("flex");
+const addResetContent = document.querySelector('#add-reset-content');
+// reusable function
+const toggleAddReset = () => {
     addReset.classList.toggle("hidden");
+    addReset.classList.toggle("flex");
+    // addReset.classList.toggle('bg-opacity-0');
+    // addReset.classList.toggle('bg-opacity-70');
+    // addResetContent.classList.toggle('translate-y-full');
+    // addResetContent.classList.toggle('translate-y-0');
+};
+const togglePopup = () => {
+    popup.classList.toggle("hidden");
+    popup.classList.toggle("flex");
+    popup.classList.toggle('bg-opacity-0');
+    popup.classList.toggle('bg-opacity-70');
+};
+//---------show add reset--------
+showAddReset.addEventListener('click', () => {
+    toggleAddReset();
 });
 //---------close add reset-------
 const addMenuDown = document.querySelector('#add-menu-down');
 addReset.addEventListener('click', e => {
     if (e.target === e.currentTarget)
-        closeAddReset();
+        toggleAddReset();
 });
-addMenuDown.addEventListener('click', () => closeAddReset());
+addMenuDown.addEventListener('click', () => toggleAddReset());
 //---------show reset pop up-------
 const resetSavingBtn = document.querySelector('#reset-saving');
 const popup = document.querySelector('#confirm-popup');
 const cancelReset = document.querySelector('#cancel-reset');
+const resetBtn = document.querySelector('.danger2');
+resetSavingBtn.addEventListener('click', () => {
+    togglePopup(); //open popup
+    popup.style.opacity = '1';
+});
 //---------cancel and close reset pop up-------
 popup.addEventListener('click', e => {
-    if (e.target === e.currentTarget) {
-        popup.classList.toggle('hidden');
-        popup.classList.toggle('flex');
-    }
+    if (e.target === e.currentTarget)
+        togglePopup();
 });
 cancelReset.addEventListener('click', () => {
-    popup.classList.toggle('hidden');
-    popup.classList.toggle('flex');
+    togglePopup();
 });
 //-------confirm reset saving and close reset pop up-------
-const resetBtn = document.querySelector('.danger2');
 resetBtn.addEventListener('click', () => {
-    closeAddReset();
-    closePopup();
-});
-resetSavingBtn.addEventListener('click', () => {
-    popup.classList.toggle('flex');
-    popup.classList.toggle('hidden');
+    toggleAddReset();
+    togglePopup();
     // code for reset saving down here
 });
