86bd6400540b5c2145c3e5ca16918921c519909c
ryrd <rdridhani@gmail.com>
2022-04-13 09:01:24 +0800

    add last 3 days feature

--- script/script.js
+++ script/script.js
@@ -6,9 +6,9 @@ let last3daysSpents = [0, 0, 0];
 //---------------------------------------------
 //---------------------------------------------
 const showAddReset = document.querySelector('#show-add');
-const addReset = document.querySelector('#add-reset');
 const addResetContent = document.querySelector('#add-reset-content');
 // reusable function
+const addReset = document.querySelector('#add-reset');
 const toggleAddReset = () => {
     addReset.classList.toggle("hidden");
     addReset.classList.toggle("flex");
@@ -33,18 +33,51 @@ const addPeriod = (x) => {
     parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     return parts.join("");
 };
+//--------------------------------------------------
+//--------------------------------------------------
+//--------------check last spent local storage and add to ------------------
 const lastSpentList = document.querySelector('#today-spents');
 const addNewSpent = (spents) => {
     const newspentlist = spents.map(spent => {
-        return `<li class="border border-white border-opacity-40 border-l-0 border-t-0 border-r-0 pl-1">${spent}</li>`;
+        return `<li class="border border-white border-opacity-40 border-l-0 border-t-0 border-r-0 pl-1">${addPeriod(spent)}</li>`;
     });
     lastSpentList.innerHTML = newspentlist.toString().replace(/,/g, "");
 };
 if (localStorage.getItem('last-spents') !== null) {
     const lastSpentsFromLocal = JSON.parse(localStorage.getItem('last-spents'));
     lastSpents.push(...lastSpentsFromLocal);
-    console.log(lastSpents);
     addNewSpent(lastSpentsFromLocal);
+}
+//--------------add last 3 days spent local storage and add to ------------------
+const last3daysSpentList = document.querySelector('#three-days-spents');
+const addthreeSpent = (spents) => {
+    const newspentlist = spents.map(spent => {
+        return `<li class="border border-white border-opacity-40 border-l-0 border-t-0 border-r-0 pl-1">${addPeriod(spent)}</li>`;
+    });
+    last3daysSpentList.innerHTML = newspentlist.toString().replace(/,/g, "");
+};
+if (localStorage.getItem('last-3days-spent') !== null) {
+    const last3daysSpentsFromLocal = JSON.parse(localStorage.getItem('last-3days-spent'));
+    last3daysSpents[0] = last3daysSpentsFromLocal[0];
+    last3daysSpents[1] = last3daysSpentsFromLocal[1];
+    last3daysSpents[2] = last3daysSpentsFromLocal[2];
+    addthreeSpent(last3daysSpents);
+}
+const today = new Date();
+if (localStorage.getItem('current-date') !== null) {
+    if (`${today.getDate()}-${today.getMonth()}` !== localStorage.getItem('current-date')) {
+        last3daysSpents[2] = last3daysSpents[1];
+        last3daysSpents[1] = last3daysSpents[0];
+        last3daysSpents[0] = lastSpents.length ? lastSpents.reduce((total, eachSpent) => total + eachSpent) : 0;
+        localStorage.setItem('current-date', `${today.getDate()}-${today.getMonth()}`);
+        localStorage.setItem('last-3days-spent', JSON.stringify(last3daysSpents));
+        //kode hapus lastSpents yesterday
+        lastSpents = [];
+        localStorage.removeItem('last-spents');
+    }
+}
+else {
+    localStorage.setItem('current-date', `${today.getDate()}-${today.getMonth()}`);
 }
 //-------------------------------------------------------------
 //-------------------------------------------------------------
--- script/script.ts
+++ script/script.ts
@@ -7,10 +7,10 @@ let last3daysSpents: [number, number, number] = [0 , 0 , 0];
 //---------------------------------------------
 
 const showAddReset = document.querySelector('#show-add') as HTMLButtonElement;
-const addReset = document.querySelector('#add-reset') as HTMLDivElement;
 const addResetContent = document.querySelector('#add-reset-content') as HTMLDivElement;
 
 // reusable function
+const addReset = document.querySelector('#add-reset') as HTMLDivElement;
 const toggleAddReset :Function = (): void => {
     addReset.classList.toggle("hidden");
     addReset.classList.toggle("flex");
@@ -39,12 +39,15 @@ const addPeriod: Function = (x: number): string => {
     return parts.join("");
 }
 
+//--------------------------------------------------
+//--------------------------------------------------
+
+//--------------check last spent local storage and add to ------------------
 const lastSpentList = document.querySelector('#today-spents') as HTMLUListElement;
 
 const addNewSpent: Function = (spents: number[]) :void => {
-    
     const newspentlist = spents.map(spent => {
-        return `<li class="border border-white border-opacity-40 border-l-0 border-t-0 border-r-0 pl-1">${spent}</li>`;
+        return `<li class="border border-white border-opacity-40 border-l-0 border-t-0 border-r-0 pl-1">${addPeriod(spent)}</li>`;
     });
 
     lastSpentList.innerHTML = newspentlist.toString().replace(/,/g, "");
@@ -53,9 +56,46 @@ const addNewSpent: Function = (spents: number[]) :void => {
 if(localStorage.getItem('last-spents') !== null){
     const lastSpentsFromLocal = JSON.parse(localStorage.getItem('last-spents')!);
     lastSpents.push(...lastSpentsFromLocal);
-    console.log(lastSpents);
     addNewSpent(lastSpentsFromLocal);
 }
+
+//--------------add last 3 days spent local storage and add to ------------------
+const last3daysSpentList = document.querySelector('#three-days-spents') as HTMLUListElement;
+const addthreeSpent: Function = (spents: number[]) :void => {
+    const newspentlist = spents.map(spent => {
+        return `<li class="border border-white border-opacity-40 border-l-0 border-t-0 border-r-0 pl-1">${addPeriod(spent)}</li>`;
+    });
+
+    last3daysSpentList.innerHTML = newspentlist.toString().replace(/,/g, "");
+}
+
+if(localStorage.getItem('last-3days-spent') !== null){
+    const last3daysSpentsFromLocal = JSON.parse(localStorage.getItem('last-3days-spent')!);
+    last3daysSpents[0] = last3daysSpentsFromLocal[0];
+    last3daysSpents[1] = last3daysSpentsFromLocal[1];
+    last3daysSpents[2] = last3daysSpentsFromLocal[2];
+    addthreeSpent(last3daysSpents);
+}
+
+const today = new Date();
+
+if(localStorage.getItem('current-date') !== null) {
+    if(`${today.getDate()}-${today.getMonth()}` !== localStorage.getItem('current-date')){
+        last3daysSpents[2] = last3daysSpents[1];
+        last3daysSpents[1] = last3daysSpents[0];
+        last3daysSpents[0] = lastSpents.length ? lastSpents.reduce((total, eachSpent) => total+eachSpent) : 0;
+        localStorage.setItem('current-date', `${today.getDate()}-${today.getMonth()}`);
+        localStorage.setItem('last-3days-spent', JSON.stringify(last3daysSpents));
+
+        //kode hapus lastSpents yesterday
+        lastSpents = [];
+        localStorage.removeItem('last-spents');
+    }   
+}
+else{
+    localStorage.setItem('current-date', `${today.getDate()}-${today.getMonth()}`);
+}
+
 
 //-------------------------------------------------------------
 //-------------------------------------------------------------
