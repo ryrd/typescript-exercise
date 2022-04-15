9241b406898268f9ad1a336bf68ce8ed9474fbf6
ryrd <rdridhani@gmail.com>
2022-04-15 09:25:46 +0800

    change duration

--- script/script.ts
+++ script/script.ts
@@ -169,7 +169,7 @@ form.addEventListener('submit', e => {
                 document.querySelector('#m')!.classList.toggle('hidden');
                 todaySpentInput.value = '';
                 todaySpentInput.blur();
-            }, 700);
+            }, 1000);
         }
         else{
             toggleMoneyLestPopup();
